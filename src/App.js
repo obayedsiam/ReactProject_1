e0974473a742bc372926e8c94e028faa0142e484
obayed <obayed@eatlbd.com>
2021-09-25 20:48:11 +0600

    toast message component  added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'reactstrap'
+import {ToastContainer, toast} from 'react-toastify'
 
 function App() {
+
+  const btnHandler = () =>{
+      toast.success("Done");
+      toast.error("Undone");
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <ToastContainer/>
+    <h1>Online Learning Platform</h1>   
+     <Button color = "primary" onClick = {btnHandler}>
+      Click 
+     </Button>
     </div>
   );
 }
 
 export default App;
+ 
