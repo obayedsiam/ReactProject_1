28a890de7767fdd303664f2859a2ddd5dfe5c419
obayed <obayed@eatlbd.com>
2021-10-31 18:57:09 +0600

    redux and saga corrected only need to add the code in the function

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import Store from './Reducers/Store';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-<Provider Store = {Store}>
+<Provider store = {Store}>
     <App />
 </Provider>, document.getElementById('root'));
 
--- src/lib/fetcher.js
+++ src/lib/fetcher.js
@@ -1,9 +1,12 @@
-//import Cookies from "js-cookies";
+import Cookies from "js-cookies";
 
 const fetcher = async (url, options = {} )=>{
 
+    const method = options.method || "get";
     if(method === "get" || method === "GET"){
-        Object.keys(options).forEach
+        Object.keys(options).forEach((key)=>{
+            url.searchParams.append(key, options[key])
+        })
     }
 
 }
